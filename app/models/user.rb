fdc36b33b21e4e1c7fe0f8542666ca47ee52f363
古賀耕介 <koga.agri.macpro@icloud.com>
2021-02-01 20:07:59 +0900

    バージョンを戻す

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :diaries, dependent: :destroy
+  has_many :diaries, dependent: :destroy
+end
+
