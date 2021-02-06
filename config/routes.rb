f9e6fd85baf74249e7fac30a0be8f7b86e8e3b3e
古賀耕介 <koga.agri.macpro@icloud.com>
2021-02-06 21:56:22 +0900

    ゲストログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
+
   root 'diaries#index'
   resources :diaries do
     resources :comments,only: [:new, :create]
