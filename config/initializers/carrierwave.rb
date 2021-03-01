a81f5aa4c9fd2100a42db3cd67b1aedb64230113
古賀耕介 <koga.agri.macpro@icloud.com>
2021-03-01 16:11:19 +0900

    error是正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,22 +5,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  # config.fog_provider = 'fog/aws'
-  config.fog_directory = 'veg-backet'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
-    path_style: true
-  }
-end
-
-if Rails.env.development?
-  storage :file
-elsif Rails.env.test?
-  storage :file
-else
-  storage :fog
+  if Rails.env.production?
+    config.storage :fog
+    # config.fog_provider = 'fog/aws'
+    config.fog_directory = 'veg-backet'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION'],
+      path_style: true
+    }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
