a951e55d89a422ed3878ca489b736f4dd008e2d6
古賀耕介 <koga.agri.macpro@icloud.com>
2021-03-01 15:59:17 +0900

    error修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.storage :fog
-  config.fog_provider = 'fog/aws'
+  # config.fog_provider = 'fog/aws'
   config.fog_directory = 'veg-backet'
   config.fog_credentials = {
     provider: 'AWS',
